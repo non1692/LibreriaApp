a41d8ea4561ca42824ef81bc8eea38fde872f698
noerojasf <146777762+noerojasf@users.noreply.github.com>
2023-12-14 09:59:01 -0600

    add publish

--- app/mylibrary/build.gradle.kts
+++ app/mylibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -29,6 +30,20 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+}
+
+publishing{
+    publications{
+        register<MavenPublication>("release") {
+            groupId = "com.github.non1692"
+            artifactId = "LibreriaApp"
+            version = "1.1.1"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
