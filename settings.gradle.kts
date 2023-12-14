e496a573452343875bc8d7419a126ab5302a7641
noerojasf <146777762+noerojasf@users.noreply.github.com>
2023-12-14 08:02:47 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LibreriaApp"
+include(":app")
+include(":app:mylibrary")
